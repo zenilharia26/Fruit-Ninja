25ec1e81833727bc445d44d88c7389438a3d3514
Zenil Haria <zenil.haria@somaiya.edu>
2019-09-04 19:47:17 +0530

    Automated fruits dictionary.

--- game.py
+++ game.py
@@ -6,44 +6,35 @@ import random
 width = 600
 height = 800
 clock = pygame.time.Clock()
+g = 1
+fruits = ['watermelon', 'orange']
+
 
 pygame.init()
 gameDisplay = pygame.display.set_mode((width, height))
 gameDisplay.fill((255,255,255))
 
-def generate_random_fruits():
-    for key,value in data.items():
-        if(random.random() >= 0.0):
-            value['throw'] = True
-        else:
-            value['throw'] = False
-
-data = {
-    'watermelon' : {
-        'img' : pygame.image.load(r'C:\\Users\\Zenil\\Documents\\CSI\\Fruit-Ninja\\watermelon.png'),
-        'x' : 100,
-        'y' : 800,
-        'speed_x' : 0,
-        'speed_y' : -80,
-        'throw' : True,
-        't' : 0
-    },
-
-    'orange' : {
-        'img' : pygame.image.load(r'C:\\Users\\Zenil\\Documents\\CSI\\Fruit-Ninja\\orange.png'),
-        'x' : 400,
-        'y' : 800,
-        'speed_x' : -5,
-        'speed_y' : -80,
-        'throw' : True,
+def generate_random_fruits(fruit):
+    data[fruit] = {
+        'img' : pygame.image.load(r'C:\\Users\\Zenil\\Documents\\CSI\\Fruit-Ninja\\'+fruit+'.png'),
+        'x' : random.randint(50, 550),
+        'y' : 800, 
+        'speed_x' : random.randint(-10, 10),
+        'speed_y' : random.randint(-101, -80),
+        'throw' : False,
         't' : 0
     }
-}
 
-g = 1
+    if(random.random() >= 0.75):
+        data[fruit]['throw'] = True
+    else:
+        data[fruit]['throw'] = False
+
+data = {}
+for fruit in fruits:
+    generate_random_fruits(fruit)
 
 pygame.display.update()
-#generate_random_fruits()
 print(data)
 
 while True:
@@ -55,18 +46,19 @@ while True:
             value['speed_y'] += (g*value['t'])
             value['t'] += 1
 
-            if value['y'] > 800:
-                value['y'] = 800
-                value['speed_y'] = -80
-                value['t'] = 0
-
-            gameDisplay.blit(value['img'], (value['x'],value['y']))
+            if value['y'] <= 800:
+                gameDisplay.blit(value['img'], (value['x'],value['y']))
+            else:
+                generate_random_fruits(key)
         
-        current_position = pygame.mouse.get_pos()
-        #print(current_position)
-        if current_position[0] > value['x'] and current_position[0] < value['x']+60 and current_position[1] > value['y'] and current_position[1] < value['y']+60:
-            value['img'] = pygame.image.load(r'C:\\Users\\Zenil\\Documents\\CSI\\Fruit-Ninja\\half_'+key+'.png')
-            value['speed_x'] += 10
+            current_position = pygame.mouse.get_pos()
+            #print(current_position)
+            if current_position[0] > value['x'] and current_position[0] < value['x']+60 and current_position[1] > value['y'] and current_position[1] < value['y']+60:
+                value['img'] = pygame.image.load(r'C:\\Users\\Zenil\\Documents\\CSI\\Fruit-Ninja\\half_'+key+'.png')
+                value['speed_x'] += 10
+        
+        else:
+            generate_random_fruits(key)
     
     pygame.display.update()
     clock.tick(13)
